0c5c0edba659904af36a5634135c65fd3f3df117
Edwin_Sukirno <esukirno@servcorp.com.au>
2014-07-07 10:07:45 +1000

    Commit first version.
    Turn on light when a build failed

---
+++ IsBambooBuildBrokenReader/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Net;
+using System.ServiceModel.Syndication;
+using System.Xml;
+using System.Configuration;
+
+namespace IsBambooBuildBrokenReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = ConfigurationManager.AppSettings["BambooBuildPlanRssFeedUrl"];
+            var username = ConfigurationManager.AppSettings["BambooUsername"];
+            var password = ConfigurationManager.AppSettings["BambooPassword"];
+
+            using (var webClient = new WebClient())
+            {
+                webClient.Credentials = new NetworkCredential(username, password);
+                var data = webClient.DownloadString(url);
+
+                using (var reader = XmlReader.Create(new StringReader(data)))
+                {
+                    var feed = SyndicationFeed.Load(reader);
+
+                    if (feed.Items.Any())
+                    {
+                        dynamic buildStatus = feed.Items.FirstOrDefault();
+                        if (buildStatus.Title.Text.Contains("FAILED"))
+                        {
+                            TurnOnSiren();
+                        }
+                        else
+                        {
+                            TurnOffSiren();
+                        }
+                    }
+                }
+            }            
+        }
+
+        static void TurnOnSiren()
+        {
+            using (var outputPort = new SerialPort("COM4", 9600))
+            {
+                outputPort.Open();
+                outputPort.Write("1");
+                outputPort.Close();
+            }
+        }
+
+        static void TurnOffSiren()
+        {
+            using (var outputPort = new SerialPort("COM4", 9600))
+            {
+                outputPort.Open();
+                outputPort.Write("0");
+                outputPort.Close();
+            }
+        }
+    }
+}
