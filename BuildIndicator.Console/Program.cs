6a91a4df3fb531454b5c1b9943cf24bde7dae15c
Edwin_Sukirno <esukirno@servcorp.com.au>
2014-07-09 11:57:42 +1000

    Renamed IsBrokenBuildreader project to BuildIndicator.Core And set BuildIndicator.Console as the console start up

--- BuildIndicator.Console/Program.cs
+++ BuildIndicator.Console/Program.cs
@@ -1,15 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Configuration;
+using IsBambooBuildBrokenReader;
 
 namespace BuildIndicator.Console
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            var planKey = ConfigurationManager.AppSettings["PlanKey"];
+            var bambooUri = ConfigurationManager.AppSettings["BambooRestApiUri"];
+
+            var notifications = new NotificationProvider(planKey, bambooUri);
+            var notifier = new CompositeBuildNotifier(new GrowlBuildNotifier(),
+                    new ArduinoBuildNotifier());
+            var checkpointer = new ResultCheckpointer();
+
+            var checkPoint = checkpointer.GetLast();
+
+            BuildNotification notification = null;
+            if (notifications.TryGetNotificationSince(checkPoint, out notification))
+            {
+                notifier.Notify(notification);
+                checkpointer.Store(notification.Checkpoint);
+            };
         }
     }
 }
